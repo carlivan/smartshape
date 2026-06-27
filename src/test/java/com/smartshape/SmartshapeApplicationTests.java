0cf68aefd73243335d743d477effead7ab2ecaa6
Carlivan <ccarlivan@yahoo.com.br>
2026-06-27 17:22:42 -0300

    chore: configuração inicial do projeto SmartShape

---
+++ src/test/java/com/smartshape/SmartshapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartshape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartshapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
